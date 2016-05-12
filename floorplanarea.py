ba73154d2b52a135302168bdfd2280c8bb56bd5a
jsb <jsb>
2016-05-12 17:58:47 +0000

    simple vertex dragging

--- floorplanarea.py
+++ floorplanarea.py
@@ -30,7 +30,7 @@ class RoomAreaOverlay(ui.View):
 		self.touch_enabled=True
 		# store list of points, and also current point for use in dragging
 		self.pts=[]
-		self.currpt=[]
+		self.curridx=-1
 		# create a textfield to set scale.  could be a slider instead
 		# scale is in units of feet/pixel, or really measurementunits/pixel
 		# it might be easier to have user enter this as 1/scale (pixel/measunit), so that it will be a number >1.  
@@ -54,19 +54,26 @@ class RoomAreaOverlay(ui.View):
 		area=polygonArea(x,y,float(self.scale.text))
 		self.lbl.text='Area: {} squnits'.format(area)
 	def touch_began(self,touch):
-		'''when starting a touch, we set the current point, and force a redraw.  but we won't add the point as final until we lift our finger.
-		If implementing an editable point function, this is where you would look through existing points to find one near current touch
+		'''when starting a touch, fiest check if there are any points very near by.  if so, set currpt to that to allow dragging prsvious points,
+		if not, add a new point.
 		'''
-		self.currpt=(touch.location.x,touch.location.y)
+		self.curridx=-1
+		currpt=(touch.location.x,touch.location.y)
+		#search existing points for a near match
+		for i,p in enumerate(self.pts):
+			if abs(ui.Point(*p)-ui.Point(*currpt))<20:
+				self.curridx=i
+		# if not match found, add a new point
+		if self.curridx==-1:
+			self.pts.append(currpt)
 		self.set_needs_display()	
 	def touch_moved(self,touch):
 		''' update the current point, and redraw'''
-		self.currpt=(touch.location.x,touch.location.y)
+		self.pts[self.curridx]=(touch.location.x,touch.location.y)
 		self.set_needs_display()	
 	def touch_ended(self,touch):
 		''' called when lifting finger.  append the final point to the permanent list of pts, then clear the current point, and redraw'''
-		self.pts.append((touch.location.x, touch.location.y))
-		self.currpt=[]
+		self.curridx=-1
 		self.set_needs_display()
 		self.update_area()
 	def draw(self):
@@ -74,9 +81,8 @@ class RoomAreaOverlay(ui.View):
 		# set color to red
 		ui.set_color((1,0,0))
 		# create a copy of self.pts, and append the current point
-		drawpts=[p for p in self.pts]
-		if self.currpt:
-			drawpts+=[self.currpt]		
+		drawpts=self.pts
+
 		if drawpts:
 			# move path to first point:
 			pth=ui.Path()
@@ -84,17 +90,16 @@ class RoomAreaOverlay(ui.View):
 			p0=drawpts[0]
 			for p in drawpts[1:]:
 				# for each point after the first, draw a line from the previous point, compute length vector L			
-				if p: # this might not be needed?
-					#draw the line segment
-					pth.line_to(*p) 
-					# compute point which is halfway along line between the start/end point.  L is the vector pointing from start to finish.  H is the Point at the halfway, referenced back to global origin
-					L=(ui.Point(*p)-ui.Point(*p0)) 
-					P0=ui.Point(*p0)
-					H=P0+L/2 #halfway point
-					# put text at the halfway point, containing length ofmsegment * scale
-					ui.draw_string('%3.2f'%abs(L*float(self.scale.text)),(H.x,H.y,0,0),color='red')
-					# set starting point for next line segment
-					p0=p
+				#draw the line segment
+				pth.line_to(*p) 
+				# compute point which is halfway along line between the start/end point.  L is the vector pointing from start to finish.  H is the Point at the halfway, referenced back to global origin
+				L=(ui.Point(*p)-ui.Point(*p0)) 
+				P0=ui.Point(*p0)
+				H=P0+L/2 #halfway point
+				# put text at the halfway point, containing length ofmsegment * scale
+				ui.draw_string('%3.2f'%abs(L*float(self.scale.text)),(H.x,H.y,0,0),color='red')
+				# set starting point for next line segment
+				p0=p
 			pth.stroke() # draw the path
 			# create a 10 pixel circle at last entered point
 			# could do this in a loop to show where 'handles' are
